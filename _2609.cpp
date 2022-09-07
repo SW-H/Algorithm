481ff4291b2e9a7462b5551d2a1a8a9bf4e67edf
SW-H <hsw08@naver.com>
2022-09-07 11:37:37 +0900

    9/7 복습

--- _2609.cpp
+++ _2609.cpp
@@ -10,7 +10,7 @@ int main() {
     return 0;
 }
 
-int GCD(int a, int b) {  // 최대공약수
+int GCD(int a, int b) { // 최대공약수
     if (b == 0) {
         return a;
     } else {
@@ -19,6 +19,6 @@ int GCD(int a, int b) {  // 최대공약수
     }
 }
 
-int LCM(int a, int b) {  // 최소공배수
+int LCM(int a, int b) { // 최소공배수
     return ((a * b) / GCD(a, b));
 }
