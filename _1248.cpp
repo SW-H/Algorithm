25e372af3e86244a2401341edd929d3edd0ef0f5
SW-H <hsw08@naver.com>
2022-10-07 23:32:58 +0900

    modify

--- _1248.cpp
+++ _1248.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 void solve(int loc);
-int factorialSum(int n);
 bool check(int loc);
-int rangeSum(int begin, int end);
 char signMatrix[56][56];
 int res[11];
 int n;
@@ -15,67 +13,47 @@ int main() {
     cout.tie(NULL);
 
     cin >> n;
+    string s;
+    cin >> s;
+
+    int idx = 0;
+
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n - i; j++) {
-            cin >> signMatrix[i][j];
-        }
-    }
-    for (int i = 1; i <= n; i++) {
-        if (signMatrix[i][i] == '-') {
-            res[i] = -11;
-        } else {
-            res[i] = 0;
+        for (int j = i; j <= n; j++) {
+            signMatrix[i][j] = s[idx++];
         }
     }
     solve(1);
 }
 
 void solve(int loc) {
-    // loc : 몇번째자리 할 차례인지 , res : 지금까지 결정된 숫자들
-    int original;
-    if (loc == n) {
+    // loc : 몇번째자리 할 차례인지, res : 지금까지 결정된 숫자들
+    if (loc == n+1 && check(loc)) {
         for (int i = 1; i <= n; i++) {
             cout << res[i] << " ";
         }
         exit(0);
         return;
     }
-    for (int i = 0; i <= 10; i++) {
-        res[loc]++;
+    for (int i = -10; i <= 10; i++) {
+        res[loc] = i;
         if (check(loc)) {
             solve(loc + 1);
         }
     }
 }
 
-int factorialSum(int n) {
-    int res = 0;
-    while (n) {
-        res += n;
-        n--;
-    }
-    return res;
-}
-
 bool check(int loc) {
-    for (int i = 1; i <= loc; i++) {
-        for (int j = 1; j <= loc; j++) {
-            if (!(signMatrix[i][j] == '-' && rangeSum(i, j) < 0) &&
-                !(signMatrix[i][j] == '+' && rangeSum(i, j) > 0) &&
-                !(signMatrix[i][j] == '0' && rangeSum(i, j) == 0)) {
-                // cout << "(" << i << "," << j << ")" << rangeSum(i, j) <<
-                // "\n";
-                return false;
-            }
+    int sum = 0;
+    for (int i = loc; i >= 0; i--) {
+        sum += res[i];
+        if ((signMatrix[i][loc] == '+' && sum <= 0) ||
+            (signMatrix[i][loc] == '-' && sum >= 0) ||
+            (signMatrix[i][loc] == '0' && sum != 0)) {
+            return false;
         }
     }
     return true;
 }
 
-int rangeSum(int begin, int end) {
-    int rangeSum = 0;
-    for (int i = begin; i <= end; i++) {
-        rangeSum += res[i];
-    }
-    return rangeSum;
-}
+// cf. https://injae-kim.github.io/problem_solving/2020/02/22/baekjoon-1248.html
