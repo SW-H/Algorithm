be8b5bb0961cec9e91883ac60bb696ff6b351658
SW-H <hsw08@naver.com>
2022-11-12 13:13:58 +0900

    수정

--- _14890.cpp
+++ _14890.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
 bool isHorizontalSlope(int (*map)[101], int row, int col);
-// bool isVerticalSlope(int row, int col);
+void solve(int (*map)[101]);
+
 int map[101][101];
 int tranposedMap[101][101];
-int N, L;
+bool slope[101][101];
+int res, N, L;
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -18,7 +21,19 @@ int main() {
             tranposedMap[j][i] = map[i][j];
         }
     }
-    int res = 0;
+
+    solve(map);
+    solve(tranposedMap);
+
+    cout << res;
+}
+
+void solve(int (*map)[101]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            slope[i][j] = false;
+        }
+    }
     bool allSame = true;
     for (int i = 0; i < N; i++) {
         allSame = true;
@@ -28,31 +43,18 @@ int main() {
             }
         }
         if (allSame) {
-            // cout << "#" << i << "\n";
-            res++;
-        }
-        allSame = true;
-        for (int j = 1; j < N; j++) {
-            if ((tranposedMap[i][j] != tranposedMap[i][j - 1]) &&
-                !isHorizontalSlope(tranposedMap, i, j)) {
-                allSame = false;
-            }
-        }
-        if (allSame) {
-            // cout << "#" << i << "\n";
             res++;
         }
     }
-
-    cout << res;
 }
 
 bool isHorizontalSlope(int (*map)[101], int row, int col) {
+    bool isGrowing = false; // F : 내리막길, T : 오르막길
     if (abs(map[row][col] - map[row][col - 1]) != 1) {
         return false;
     }
     if (map[row][col] < map[row][col - 1]) { // 내리막길
-        if ((col + L < N) && (map[row][col] != map[row][col + L - 1])) {
+        if ((col + L - 1 < N) && (map[row][col] != map[row][col + L - 1])) {
             return false;
         }
         for (int i = 1; i < L; i++) {
@@ -61,41 +63,37 @@ bool isHorizontalSlope(int (*map)[101], int row, int col) {
             }
         }
     } else { // 오르막길
+        isGrowing = true;
         if (col < L) {
             return false;
         }
-        for (int i = 1; i <= L; i++) {
+        for (int i = 1; i < L; i++) {
             if (col - i < 0 || (map[row][col - 1 - i] != map[row][col - 1])) {
                 return false;
             }
         }
     }
+    if (isGrowing) {
+        for (int i = 1; i <= L; i++) {
+            if (slope[row][col - i]) {
+                return false;
+            }
+        }
+    } else {
+        for (int i = 0; i < L; i++) {
+            if (slope[row][col + i]) {
+                return false;
+            }
+        }
+    }
+    if (isGrowing) {
+        for (int i = 1; i <= L; i++) {
+            slope[row][col - i] = true;
+        }
+    } else {
+        for (int i = 0; i < L; i++) {
+            slope[row][col + i] = true;
+        }
+    }
     return true;
 }
-
-// bool isVerticalSlope(int row, int col) {
-//     if (abs(map[row][col] - map[row][col - 1]) != 1) {
-//         return false;
-//     }
-//     if (map[row][col] < map[row][col - 1]) { // 내리막길
-//         if ((col + L < N) && (map[row][col] != map[row][col + L - 1])) {
-//             return false;
-//         }
-//         for (int i = 1; i < L; i++) {
-//             if (col + i >= N || map[row][col + i] != map[row][col]) {
-//                 return false;
-//             }
-//         }
-//     } else { // 오르막길
-//         if (col < L) {
-//             return false;
-//         }
-//         for (int i = 1; i <= L; i++) {
-//             if (col - i < 0 || (map[row][col - 1 - i] != map[row][col - 1]))
-//             {
-//                 return false;
-//             }
-//         }
-//     }
-//     return true;
-// }
