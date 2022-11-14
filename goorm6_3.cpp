16258b8eb53b7785f47d86a8d9c7e80b26b729ed
SW-H <hsw08@naver.com>
2022-11-14 18:56:08 +0900

    add

--- goorm6_3.cpp
+++ goorm6_3.cpp
@@ -55,3 +55,36 @@ void decrypt(string token) {
         }
     }
 }
+
+
+/*
+예시 답안
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N = 1e6+100;
+char s[N], f, k[N];
+int main(){
+    int T;
+    scanf("%d",&T);
+    while(T--){
+        scanf("\n%[^\n]\n%c %[^\n]",s,&f,k);
+        if(f=='E'){
+            for(int i=0, j=0; s[i]; ++i, ++j){
+                if(!k[j]) j=0;
+                if('a'<=s[i] && s[i]<='z') s[i]=(s[i]+k[j]-'a')%26+'a';
+                if('A'<=s[i] && s[i]<='Z') s[i]=(s[i]+k[j]-'A')%26+'A';
+            }
+        }
+        else{
+            for(int i=0, j=0; s[i]; ++i, ++j){
+                if(!k[j]) j=0;
+                if('a'<=s[i] && s[i]<='z') s[i]=(s[i]+(26-k[j]%26)-'a')%26+'a';
+                if('A'<=s[i] && s[i]<='Z') s[i]=(s[i]+(26-k[j]%26)-'A')%26+'A';
+            }
+        }
+        puts(s);
+    }
+}
+*/
